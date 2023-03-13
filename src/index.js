bc3f7268f48a971e5241a169fd4d5eb6b61b5857
tanersaydam <tanersaydam@gmail.com>
2023-03-13 07:32:49 +0300

    .

--- src/index.js
+++ src/index.js
@@ -31,6 +31,96 @@ function XoxGameComponent(){
     setMessage("Hamle Sırası: " + mark);
   }
 
+  const markGame = (index) => {
+    if(!isGameFinish){
+    const newGames = [...games];
+    if(newGames[index] == ""){
+      newGames[index] = mark;
+      setGames(newGames);
+
+      let e = isMoveFinish(newGames);
+      if(e){
+        setMessage("Oyun berabere");
+        setIsGameFinish(true);
+        return;
+      }
+
+      let r = isGameOver(newGames);
+      if(r){
+        setMessage("Oyunu " + mark + " kazandı!");
+        setIsGameFinish(true);
+        return;
+      }
+
+      mark == "X" ? setMark("O") : setMark("X");
+      setMessage("Hamle Sırası: " + (mark == "X"  ? 'O' : 'X'))
+    }
+  }
+  }
+
+  const isGameOver = (newGames) => {
+    if(newGames[0] != "" 
+    && newGames[0] === newGames[1] 
+    && newGames[1] === newGames[2]){
+      return true
+    }
+
+    if(newGames[3] != "" 
+    && newGames[3] === newGames[4] 
+    && newGames[4] === newGames[5]){
+      return true
+    }
+
+    if(newGames[6] != "" 
+    && newGames[6] === newGames[7] 
+    && newGames[7] === newGames[8]){
+      return true
+    }
+
+    if(newGames[0] != "" 
+    && newGames[0] === newGames[3] 
+    && newGames[3] === newGames[6]){
+      return true
+    }
+
+    if(newGames[1] != "" 
+    && newGames[1] === newGames[4] 
+    && newGames[4] === newGames[7]){
+      return true
+    }
+
+    if(newGames[2] != "" 
+    && newGames[2] === newGames[5] 
+    && newGames[5] === newGames[8]){
+      return true
+    }
+
+    if(newGames[0] != "" 
+    && newGames[0] === newGames[4] 
+    && newGames[4] === newGames[8]){
+      return true
+    }
+
+    if(newGames[2] != "" 
+    && newGames[2] === newGames[4] 
+    && newGames[4] === newGames[6]){
+      return true
+    }
+
+    return false;
+  }
+
+  function isMoveFinish(newGames){
+    for (let i = 0; i < newGames.length; i++) {
+      const element = newGames[i];
+      if(element === ""){
+        return false;
+      }
+    }
+
+    return true;
+  }
+
   return(
     <>
     <div className='container text-center'>
@@ -38,14 +128,15 @@ function XoxGameComponent(){
       <h2 className='alert alert-warning'>
         {message}
       </h2>
-      <button className='btn btn-outline-primary w-100'>
+      <button onClick={newGame} className='btn btn-outline-primary w-100'>
         Yeni Oyun
       </button>
       <div className='row mt-2'>
         {games.map((game, index) => (
           <div
           key={index}
-          className="col-md-4 box">
+          className="col-md-4 box"
+          onClick={()=> markGame(index)}>
             {game}
           </div>
         ))}
